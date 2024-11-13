8edc8d4c8a69baab27b71087bb5c091110ccf2e3
Martial Denver <apdenvermartial@gmail.com>
2024-11-13 11:59:54 +0100

    Page update

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
+  // Supprimez transpilePackages s'il ne contient que ces packages
   webpack: (config) => {
-    // Config existante
     config.experiments = {
       ...config.experiments,
       topLevelAwait: true,
     }
-    
-    // Ajout de la config pour canvas
-    config.externals = [...(config.externals || []), { canvas: 'canvas' }]
-    
     return config
   },
 };
