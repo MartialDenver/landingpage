a081cb2e863ffd66812aad05482ec14c4716d07c
Martial Denver <apdenvermartial@gmail.com>
2026-06-22 16:06:17 +0100

    deployement update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  // Ajout de cette ligne pour désactiver le bug des workers Turbopack sur Vercel
+  turbopack: {},
   // Supprimez transpilePackages s'il ne contient que ces packages
   webpack: (config) => {
     config.experiments = {
